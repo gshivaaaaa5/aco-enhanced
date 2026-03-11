108b2eee06f6dd7fe4a02b6e10d6e544c96132ff
gshiva <gshivadev@gmail.com>
2026-03-11 17:49:41 +0530

    Initial commit

--- scratch/aco-test.cc
+++ scratch/aco-test.cc
@@ -27,6 +27,8 @@ int main (int argc, char *argv[]) {
     cmd.AddValue ("nNodes", "Number of drones", nNodes);
     cmd.Parse (argc, argv);
     
+   
+    
     if (nNodes < 6) {
         std::cout << "Error: Please use at least 6 nodes to support 3 distinct flows." << std::endl;
         return 1;
@@ -59,23 +61,23 @@ int main (int argc, char *argv[]) {
     Ipv4InterfaceContainer interfaces = address.Assign (devices); // This creates the missing 'interfaces'
 
  // --- 1. DYNAMIC GRID & BOUNDS MATH ---
-    uint32_t gridWidth = std::ceil(std::sqrt(nNodes)); 
-    double mapSize = gridWidth * 50.0; // <--- Changed from 60.0 to 80.0
-    if (mapSize < 400.0) mapSize = 400.0;
-
+    // --- 1. FIXED MAP BOUNDARIES ---
+    // Matches the IEEE Paper Table II Roadmap Size: 700m x 700m
+    
     MobilityHelper mobility;
-    mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
-                               "MinX", DoubleValue (0.0), "MinY", DoubleValue (0.0),
-                               "DeltaX", DoubleValue (60.0), "DeltaY", DoubleValue (60.0),
-                               "GridWidth", UintegerValue (gridWidth), "LayoutType", StringValue ("RowFirst"));
-                               
-    mobility.SetMobilityModel ("ns3::GaussMarkovMobilityModel",
-      "Bounds", BoxValue (Box (0, mapSize, 0, mapSize, 0, 150)),
-      "TimeStep", TimeValue (Seconds (0.5)),
-      "Alpha", DoubleValue (0.85),
-      "MeanVelocity", StringValue ("ns3::UniformRandomVariable[Min=5|Max=15]"));
+    
+    // Position drones randomly within the 700x700 map
+    mobility.SetPositionAllocator ("ns3::RandomRectanglePositionAllocator",
+                                   "X", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=700.0]"),
+                                   "Y", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=700.0]"));
+    
+    // Set speed range 0-100km/h (approx 27.7 m/s)
+    mobility.SetMobilityModel ("ns3::RandomWaypointMobilityModel",
+                               "Speed", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=27.7]"),
+                               "Pause", StringValue ("ns3::ConstantRandomVariable[Constant=2.0]"),
+                               "PositionAllocator", StringValue ("ns3::RandomRectanglePositionAllocator"));
+    
     mobility.Install (nodes);
-
     // --- 2. DYNAMIC TRAFFIC FLOW ASSIGNMENTS ---
     uint32_t s1 = nNodes - 1;
     uint32_t s2 = nNodes - 2;
@@ -86,45 +88,45 @@ int main (int argc, char *argv[]) {
 
     // Flow 1
     UdpEchoServerHelper echoServer1 (9);
-    ApplicationContainer serverApps1 = echoServer1.Install (nodes.Get (s1));
+    ApplicationContainer serverApps1 = echoServer1.Install (nodes.Get (nNodes - 1));
     serverApps1.Start (Seconds (1.0));
-    serverApps1.Stop (Seconds (20.0));
+    serverApps1.Stop (Seconds (400.0));
 
-    UdpEchoClientHelper echoClient1 (interfaces.GetAddress (s1), 9);
-    echoClient1.SetAttribute ("MaxPackets", UintegerValue (100000));
-    echoClient1.SetAttribute ("Interval", TimeValue (Seconds (0.1))); 
-    echoClient1.SetAttribute ("PacketSize", UintegerValue (1024));
-    ApplicationContainer clientApps1 = echoClient1.Install (nodes.Get (c1));
-    clientApps1.Start (Seconds (2.0));
-    clientApps1.Stop (Seconds (55.0));
+    UdpEchoClientHelper echoClient1 (interfaces.GetAddress (nNodes - 1), 9);
+    echoClient1.SetAttribute ("MaxPackets", UintegerValue (10000));
+    echoClient1.SetAttribute ("Interval", TimeValue (Seconds (0.25)));
+    echoClient1.SetAttribute ("PacketSize", UintegerValue (512));
+    ApplicationContainer clientApps1 = echoClient1.Install (nodes.Get (0));
+    clientApps1.Start (Seconds (10.0));
+    clientApps1.Stop (Seconds (390.0));
     
     // Flow 2
     UdpEchoServerHelper echoServer2(10);
-    ApplicationContainer serverApps2 = echoServer2.Install(nodes.Get(s2));
-    serverApps2.Start(Seconds(1.0));
-    serverApps2.Stop(Seconds(20.0));
+    ApplicationContainer serverApps2 = echoServer2.Install (nodes.Get (nNodes - 2));
+    serverApps2.Start (Seconds (1.0));
+    serverApps2.Stop (Seconds (400.0));
 
-    UdpEchoClientHelper echoClient2(interfaces.GetAddress(s2), 10);
-    echoClient2.SetAttribute("MaxPackets", UintegerValue(100000));
-    echoClient2.SetAttribute("Interval", TimeValue(Seconds(0.1)));
-    echoClient2.SetAttribute("PacketSize", UintegerValue(1024));
-    ApplicationContainer clientApps2 = echoClient2.Install(nodes.Get(c2));
-    clientApps2.Start(Seconds(2.0));
-    clientApps2.Stop(Seconds(55.0));
+   UdpEchoClientHelper echoClient2 (interfaces.GetAddress (nNodes - 2), 10);
+    echoClient2.SetAttribute ("MaxPackets", UintegerValue (10000));
+    echoClient2.SetAttribute ("Interval", TimeValue (Seconds (0.25)));
+    echoClient2.SetAttribute ("PacketSize", UintegerValue (512));
+    ApplicationContainer clientApps2 = echoClient2.Install (nodes.Get (1));
+    clientApps2.Start (Seconds (11.0));
+    clientApps2.Stop (Seconds (390.0));
 
     // Flow 3
     UdpEchoServerHelper echoServer3(11);
-    ApplicationContainer serverApps3 = echoServer3.Install(nodes.Get(s3));
-    serverApps3.Start(Seconds(1.0));
-    serverApps3.Stop(Seconds(20.0));
+    ApplicationContainer serverApps3 = echoServer3.Install (nodes.Get (nNodes - 3));
+    serverApps3.Start (Seconds (1.0));
+    serverApps3.Stop (Seconds (400.0));
 
-    UdpEchoClientHelper echoClient3(interfaces.GetAddress(s3), 11);
-    echoClient3.SetAttribute("MaxPackets", UintegerValue(100000));
-    echoClient3.SetAttribute("Interval", TimeValue(Seconds(0.1)));
-    echoClient3.SetAttribute("PacketSize", UintegerValue(1024));
-    ApplicationContainer clientApps3 = echoClient3.Install(nodes.Get(c3));
-    clientApps3.Start(Seconds(2.0));
-    clientApps3.Stop(Seconds(55.0));
+    UdpEchoClientHelper echoClient3 (interfaces.GetAddress (nNodes - 3), 11);
+    echoClient3.SetAttribute ("MaxPackets", UintegerValue (10000));
+    echoClient3.SetAttribute ("Interval", TimeValue (Seconds (0.25)));
+    echoClient3.SetAttribute ("PacketSize", UintegerValue (512));
+    ApplicationContainer clientApps3 = echoClient3.Install (nodes.Get (2));
+    clientApps3.Start (Seconds (12.0));
+    clientApps3.Stop (Seconds (390.0));
 
     FlowMonitorHelper flowmon;
     Ptr<FlowMonitor> monitor = flowmon.InstallAll();
